cc482cbf2811819ac1492a6d9cbb0719941279f9
au1rey <saidelaudrey@gmail.com>
2025-02-11 20:03:15 -0500

    Added navigation for Home, Journals, and Journal Edit

--- app.py
+++ app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
+
+@app.route('/journals')
+def journals():
+    return render_template('journals.html')
+
+@app.route('/edit')
+def edit():
+    return render_template('edit.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
